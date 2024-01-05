f8b0bdeeacd8922562fb422494ab1e5487818fa4
Chad-Current <30292814+Chad-Current@users.noreply.github.com>
2024-01-05 08:59:13 -0600

    Create billing.py

---
+++ Capstone Project/billing.py
@@ -0,0 +1,8 @@
+def main():
+  import pandas as pd
+  df = pd.read_csv('C:\\Users\\ccurrent\\OneDrive - Iowa Department of Public Safety\\Documents\\Detail_BillingAccount_3fe80514-3816-5040-5454-d24f07618466_5975f32c-d236-47ec-9301-bc1fd78b0421_2019-05-31_202401_en.csv',index_col='0')
+  df.head()
+
+if __name__=="billing":
+  def main()
+  
